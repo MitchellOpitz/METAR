47679b65e576e1c4c4d0ec63f763e6155a26bdc5
Mitchell Opitz <mitchell@mitchellopitz.net>
2024-03-04 09:16:38 -0600

    fix - Pins definitions to constants

--- Config.h
+++ Config.h
@@ -1,13 +1,13 @@
 const int NUM_LEDS = 25;
-#define LOOP_INTERVAL 300000
-#define TRIGGER_PIN D4
-#define DATA_PIN D2
+const int LOOP_INTERVAL = 300000;
+const int TRIGGER_PIN = D4;
+const int DATA_PIN = D2;
+const int POT_PIN = A0;
+const int REPROGRAM_BUTTON_PIN = D3;
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 #define SERVER "aviationweather.gov"
 #define BASE_URI "/api/data/dataserver.php?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecentForEachStation=constraint&stationString="
-#define POT_PIN A0
-#define REPROGRAM_BUTTON_PIN D3
 
 int timeout = 120;
 String airports = "";
